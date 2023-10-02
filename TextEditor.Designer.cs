98b86f86e761fa7cfbfcbcf9a3382b4303709936
Cavan DeForrest <88917345+Robert-DeForrest-Reynolds@users.noreply.github.com>
2023-10-02 15:34:50 -0700

    Add project files.

---
+++ TextEditor.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TextEditor
+{
+    partial class TextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.Toolbar = new System.Windows.Forms.MenuStrip();
+            this.generalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createNewNoteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.DirectoryListing = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Toolbar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Toolbar
+            // 
+            this.Toolbar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generalToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.Toolbar.Location = new System.Drawing.Point(0, 0);
+            this.Toolbar.Name = "Toolbar";
+            this.Toolbar.Size = new System.Drawing.Size(800, 24);
+            this.Toolbar.TabIndex = 1;
+            this.Toolbar.Text = "Toolbar";
+            // 
+            // generalToolStripMenuItem
+            // 
+            this.generalToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openDirectoryToolStripMenuItem,
+            this.createNewNoteToolStripMenuItem});
+            this.generalToolStripMenuItem.Name = "generalToolStripMenuItem";
+            this.generalToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.generalToolStripMenuItem.Text = "General";
+            // 
+            // openDirectoryToolStripMenuItem
+            // 
+            this.openDirectoryToolStripMenuItem.Name = "openDirectoryToolStripMenuItem";
+            this.openDirectoryToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openDirectoryToolStripMenuItem.Text = "Open Directory";
+            this.openDirectoryToolStripMenuItem.Click += new System.EventHandler(this.OpenDirectory);
+            // 
+            // createNewNoteToolStripMenuItem
+            // 
+            this.createNewNoteToolStripMenuItem.Name = "createNewNoteToolStripMenuItem";
+            this.createNewNoteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createNewNoteToolStripMenuItem.Text = "Create New Note";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(126, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(662, 394);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // DirectoryListing
+            // 
+            this.DirectoryListing.FormattingEnabled = true;
+            this.DirectoryListing.Location = new System.Drawing.Point(0, 27);
+            this.DirectoryListing.Name = "DirectoryListing";
+            this.DirectoryListing.Size = new System.Drawing.Size(120, 394);
+            this.DirectoryListing.TabIndex = 3;
+            // 
+            // TextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DirectoryListing);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.Toolbar);
+            this.MainMenuStrip = this.Toolbar;
+            this.Name = "TextEditor";
+            this.Text = "Text Editor";
+            this.Toolbar.ResumeLayout(false);
+            this.Toolbar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip Toolbar;
+        private System.Windows.Forms.ToolStripMenuItem generalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createNewNoteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox DirectoryListing;
+        private System.Windows.Forms.ToolStripMenuItem openDirectoryToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ TextEditor.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+    public partial class TextEditor : Form
+    {
+        public string[] ContentNames;
+        public string DirectoryPath;
+        public string UserFolder;
+        public bool UserFolderExists;
+
+        public TextEditor()
+        {
+            UserFolder = "UserFolder";
+            UserFolderExists = Directory.Exists(UserFolder);
+
+            if (!UserFolderExists)
+            {
+                Directory.CreateDirectory(UserFolder);
+            }
+
+            InitializeComponent();
+        }
+
+        public void Load_Directory_Items(string DirectoryPath)
+        {
+            ContentNames = Directory.GetFiles(DirectoryPath);
+
+            foreach (string Content in ContentNames)
+            {
+                DirectoryListing.Items.Add(Path.GetFileName(Content));
+            }
+        }
+
+        public void Load_User_Items()
+        {
+            string[] UserContent = Directory.GetFiles(UserFolder);
+        }
+
+        private void OpenDirectory(object sender, System.EventArgs e)
+        {
+            using (FolderBrowserDialog OpenFolder = new FolderBrowserDialog())
+            {
+                if (OpenFolder.ShowDialog() == DialogResult.OK)
+                {
+                    DirectoryPath = OpenFolder.SelectedPath;
+                }
+                else
+                {
+                    DirectoryPath = string.Empty;
+                }
+            }
+
+            Load_Directory_Items(DirectoryPath);
+
+
+        }
+    }
+}
